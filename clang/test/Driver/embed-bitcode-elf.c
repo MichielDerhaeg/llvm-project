57d5a9edb624b2d4c818df56660a2a4d899469f6
Michiel Derhaeg <michiel@derhaeg.be>
2022-05-24 21:34:01 +0200

    add target requirement to lit test

--- clang/test/Driver/embed-bitcode-elf.c
+++ clang/test/Driver/embed-bitcode-elf.c
@@ -1,10 +1,13 @@
+// REQUIRES: aarch64-registered-target
+
 // RUN: %clang -c -target aarch64-linux-android21 %s -fembed-bitcode -o %t.o \
 // RUN: -ffunction-sections -fdata-sections -fstack-protector-strong
+
 // RUN: llvm-readobj -S %t.o | FileCheck --check-prefix=CHECK %s
 // CHECK:   Name: .text.foo
 // CHECK:   Name: .llvmbc
 // CHECK:   Name: .llvmcmd
-//
+
 // RUN: llvm-objcopy %t.o --dump-section=.llvmcmd=%t.llvmcmd
 // RUN: FileCheck --check-prefix=CMD %s < %t.llvmcmd
 // CMD: ffunction-sections
